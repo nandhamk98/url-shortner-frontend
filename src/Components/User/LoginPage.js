a88cc8672d1cc3015ee2145f17d02ecebc1cfe35
Nandhakumar <nandhakumarmk98@gmail.com>
2022-07-11 03:10:46 +0530

    created login page

--- src/Components/User/LoginPage.js
+++ src/Components/User/LoginPage.js
@@ -1,3 +1,30 @@
+import "./user.css";
+
 export function LoginPage() {
-  return <div className="loginPageContainer"></div>;
+  return (
+    <div className="loginPageContainer">
+      <div className="loginContainer">
+        <h1>Login</h1>
+        <div className="userInput">
+          <label for="username" className="userInputLabel">
+            Username<span className="mandatory">*</span>
+          </label>
+          <input type="text" id="username" required />
+        </div>
+        <div className="userInput">
+          <label for="password" className="userInputLabel">
+            Password<span className="mandatory">*</span>
+          </label>
+          <input type="password" id="password" required />
+        </div>
+        <div className="buttonGroup">
+          <button className="loginBtn">Login</button>
+        </div>
+        <div className="otherOptions">
+          <p>forgot password</p>
+          <p>Create a new account</p>
+        </div>
+      </div>
+    </div>
+  );
 }
